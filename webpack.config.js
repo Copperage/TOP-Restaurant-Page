90e58de282ce5c64c04aaf2d276e86524abbbfcb
Copperage <ethanhud6@gmail.com>
2023-06-14 13:27:41 +0100

    scss and style webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src/index.js'),
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
 	},
 };
