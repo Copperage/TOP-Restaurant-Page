b196513671e67d1659e193ce4ba7136a8d0d1a3c
Copperage <ethanhud6@gmail.com>
2023-10-27 16:55:11 +0100

    webpack formatting error

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,19 @@ module.exports = {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader', 'file-loader'],
-				options: {
-					name: '[name].[ext]',
-					outputPath: 'fonts/',
-				},
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/',
+						},
+					},
+				],
 			},
 		],
 	},
